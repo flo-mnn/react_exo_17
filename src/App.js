29679e6935fca0d54f7f0cc8ef464b5f8061a257
Florence <monnaie.florence@gmail.com>
2021-01-19 14:53:16 +0100

    first btn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-// import React, {useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [number,setNumber] = useState(0);
+
   return (
     <div className="App">
+      <h1>{number}</h1>
+      <button onClick={()=> setNumber(number+1)}>+1</button>
     </div>
   );
 }
